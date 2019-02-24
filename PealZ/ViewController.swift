d7f92253a036b1f29c3195fe271a06dd36d00c16
Dylan Rothfeld <drothfel@gmail.com>
2019-02-23 19:08:14 -0800

    started diet parameter view controller storyboard with diet type picker selector

--- PealZ/ViewController.swift
+++ PealZ/ViewController.swift
@@ -8,12 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    // UI Outlets
+    @IBOutlet weak var DietFocusPicker: UIPickerView!
+    
+    // Controller Values
+    var dietFocusPickerData: [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        interfaceSetup()
     }
-
-
+    
+    // Picker column count
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // Picker row count
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dietFocusPickerData.count
+    }
+    
+    // Display the picker option labels
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return dietFocusPickerData[row]
+    }
+    
+    // Capture the picker view selection
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(dietFocusPickerData[row])
+    }
+    
+    // Perform any additional UI setup
+    func interfaceSetup() {
+        self.DietFocusPicker.delegate = self
+        self.DietFocusPicker.dataSource = self
+        dietFocusPickerData = ["Normal Balance", "Heavy Protein", "Extra Fiber", "Low Fat", "Carbo-Loading"]
+    }
 }
 
