47c25693e3cfd5357eb56123e2fcc607e75cf209
Dylan Rothfeld <drothfel@gmail.com>
2019-02-26 16:12:10 -0800

    finished nutritionix api response data parsing into local model

---
+++ PealZ/Constants/MealItems.swift
@@ -0,0 +1,9 @@
+//
+//  MealItems.swift
+//  PealZ
+//
+//  Created by Dylan Rothfeld on 2/26/19.
+//  Copyright © 2019 Dylan Rothfeld. All rights reserved.
+//
+
+let meal_item_error: MealItem = MealItem(name: "ERROR", quantity: 0, totalCarbs: 0, totalFat: 0, totalCalories: 0, totalProtein: 0, totalFiber: 0, totalSugar: 0)
--- PealZ/View Controllers/GeneratedMealPlanViewController.swift
+++ PealZ/View Controllers/GeneratedMealPlanViewController.swift
@@ -50,7 +50,8 @@ class GeneratedMealPlanViewController: UIViewController {
         parseFoodPreferenceData()
     
         // TESTING NUTRITIONIX API CALL METHOD
-        let _ = getMealItemNutritionData(item: "avacado toast")
+//        var testMealItem = getMealItemNutritionData(item: "avocado toast")
+//        testMealItem.incrementQuantity()
     }
     
     // Parse the breakfast/lunch/dinner food preferences raw data
@@ -63,6 +64,7 @@ class GeneratedMealPlanViewController: UIViewController {
     // Makes an API call to Nutritionix to gather
     // all nutritional data for meal item
     func getMealItemNutritionData(item: String) -> MealItem {
+        var capturedMealItem: MealItem? = nil
         // API URL
         let url = URL(string: "https://trackapi.nutritionix.com/v2/natural/nutrients")
         let unwrappedURL = url!
@@ -86,14 +88,101 @@ class GeneratedMealPlanViewController: UIViewController {
                 print(error?.localizedDescription ?? "No data")
                 return
             }
+            
             let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
             if let responseJSON = responseJSON as? [String: Any] {
-                print(responseJSON)
+                print(responseJSON.description)
+                var capturedCarbs = 0
+                var capturedFat = 0
+                var capturedCalories = 0
+                var capturedProtein = 0
+                var capturedFiber = 0
+                var capturedSugar = 0
+                
+                var currentSubstring: String = ""
+                
+                var foundCarbData = 0
+                var foundFatData = 0
+                var foundCaloriesData = 0
+                var foundProteinData = 0
+                var foundFiberData = 0
+                var foundSugarData = 0
+                
+                // Parsing returned JSON to create MealItem object
+                for (_, char) in responseJSON.description.enumerated() {
+                    // Found carb field
+                    if (foundCarbData == 0 && currentSubstring.trimmingCharacters(in: .whitespaces) == "\"nf_total_carbohydrate\"") { foundCarbData = 1 }
+                    // Found fat field
+                    if (foundFatData == 0 && currentSubstring.trimmingCharacters(in: .whitespaces) == "\"nf_total_fat\"") { foundFatData = 1 }
+                    // Found calories field
+                    if (foundCaloriesData == 0 && currentSubstring.trimmingCharacters(in: .whitespaces) == "\"nf_calories\"") { foundCaloriesData = 1 }
+                    // Found protein field
+                    if (foundProteinData == 0 && currentSubstring.trimmingCharacters(in: .whitespaces) == "\"nf_protein\"") { foundProteinData = 1 }
+                    // Found fiber field
+                    if (foundFiberData == 0 && currentSubstring.trimmingCharacters(in: .whitespaces) == "\"nf_dietary_fiber\"") { foundFiberData = 1 }
+                    // Found sugar field
+                    if (foundSugarData == 0 && currentSubstring.trimmingCharacters(in: .whitespaces) == "\"nf_sugars\"") { foundSugarData = 1 }
+                    
+                    // Reset substring
+                    if (char == " ") {
+                        // Get carb field data
+                        if (foundCarbData < 3 && foundCarbData > 0) { foundCarbData += 1 } else if (foundCarbData == 3) { foundCarbData = 4
+                            let characters = Array(currentSubstring.dropLast())
+                            var decodedString = ""
+                            for (index, value) in characters.enumerated() { if (index > 1 && index < characters.count - 2) { decodedString += String(value) } }
+                            capturedCarbs = Int(Float(decodedString)!)
+                        }
+                        // Get fat field data
+                        if (foundFatData < 3 && foundFatData > 0) { foundFatData += 1 } else if (foundFatData == 3) { foundFatData = 4
+                            let characters = Array(currentSubstring.dropLast())
+                            var decodedString = ""
+                            for (index, value) in characters.enumerated() { if (index > 1 && index < characters.count - 2) { decodedString += String(value) } }
+                            capturedFat = Int(Float(decodedString)!)
+                        }
+                        // Get calories field data
+                        if (foundCaloriesData < 3 && foundCaloriesData > 0) { foundCaloriesData += 1 } else if (foundCaloriesData == 3) { foundCaloriesData = 4
+                            let characters = Array(currentSubstring.dropLast())
+                            var decodedString = ""
+                            for (index, value) in characters.enumerated() { if (index > 1 && index < characters.count - 2) { decodedString += String(value) } }
+                            capturedCalories = Int(Float(decodedString)!)
+                        }
+                        // Get protein field data
+                        if (foundProteinData < 3 && foundProteinData > 0) { foundProteinData += 1 } else if (foundProteinData == 3) { foundProteinData = 4
+                            let characters = Array(currentSubstring.dropLast())
+                            var decodedString = ""
+                            for (index, value) in characters.enumerated() { if (index > 1 && index < characters.count - 2) { decodedString += String(value) } }
+                            capturedProtein = Int(Float(decodedString)!)
+                        }
+                        // Get fiber field data
+                        if (foundFiberData < 3 && foundFiberData > 0) { foundFiberData += 1 } else if (foundFiberData == 3) { foundFiberData = 4
+                            let characters = Array(currentSubstring.dropLast())
+                            var decodedString = ""
+                            for (index, value) in characters.enumerated() { if (index > 1 && index < characters.count - 2) { decodedString += String(value) } }
+                            capturedFiber = Int(Float(decodedString)!)
+                        }
+                        // Get sugar field data
+                        if (foundSugarData < 3 && foundSugarData > 0) { foundSugarData += 1 } else if (foundSugarData == 3) { foundSugarData = 4
+                            let characters = Array(currentSubstring.dropLast())
+                            var decodedString = ""
+                            for (index, value) in characters.enumerated() { if (index > 1 && index < characters.count - 2) { decodedString += String(value) } }
+                            capturedSugar = Int(Float(decodedString)!)
+                        }
+                        
+                        currentSubstring = ""
+                    }
+                    
+                    // Add on to the current substring
+                    currentSubstring += String(char)
+                }
+                
+                // Creating local MealItem object based on captured data from API call
+                capturedMealItem = MealItem(name: item, quantity: 1, totalCarbs: capturedCarbs, totalFat: capturedFat, totalCalories: capturedCalories, totalProtein: capturedProtein, totalFiber: capturedFiber, totalSugar: capturedSugar)
             }
         }
         task.resume()
         
-        return MealItem(name: "NULL", quantity: 1, totalCarbs: 0, totalFat: 0, totalCalories: 0, totalProtein: 0, totalFiber: 0, totalSugar: 0)
+        return capturedMealItem ?? meal_item_error
+        
     }
     
     // The Reset button is pressed
