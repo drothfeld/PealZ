bc0da4587f90c052505f656b1189aa6e6c060265
Dylan Rothfeld <drothfel@gmail.com>
2019-02-27 10:53:35 -0800

    generating breakfast, lunch, and dinner meal plans

--- PealZ/View Controllers/GeneratedMealPlanViewController.swift
+++ PealZ/View Controllers/GeneratedMealPlanViewController.swift
@@ -48,12 +48,86 @@ class GeneratedMealPlanViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Handle user food preference input data
         parseFoodPreferenceData()
         allMealItemPermutationsList = getAllMealItemPermutations(breakfast: breakfastFoodsList, lunch: lunchFoodsList, dinner: dinnerFoodsList)
+        
+        // Procedurally generate a new meal plan
+//        generateBreakfast()
+//        generateLunch()
+//        generateDinner()
+        
+        // Update the user interface
+        //
+        // TODO: Need a new function to update
+        //       UI values based on generated plan.
+        //
+    }
     
-        // TESTING NUTRITIONIX API CALL METHOD
-//        var testMealItem = getMealItemNutritionData(item: "avocado toast")
-//        testMealItem.incrementQuantity()
+    // Generate a meal plan for breakfast
+    func generateBreakfast() {
+        var breakfastCalories = 0
+        for _ in 0...20 {
+            if (breakfastCalories > (Int(calorieGoal)!/3)) {
+                return
+            }
+            
+            let newBreakfastItem = getMealItemNutritionData(item: allMealItemPermutationsList[0][Int(arc4random_uniform(UInt32(allMealItemPermutationsList[0].count)) + 0)])
+            
+            if (newBreakfastItem.totalCalories > 0) {
+                currentCarbs += newBreakfastItem.totalCarbs
+                currentFat += newBreakfastItem.totalFat
+                currentCalories += newBreakfastItem.totalCalories
+                currentProtein += newBreakfastItem.totalProtein
+                currentFiber += newBreakfastItem.totalFiber
+                breakfastCalories += newBreakfastItem.totalCalories
+                finalBreakfast.append(newBreakfastItem)
+            }
+        }
+    }
+    
+    // Generate a meal plan for lunch
+    func generateLunch() {
+        var lunchCalories = 0
+        for _ in 0...20 {
+            if (lunchCalories > (Int(calorieGoal)!/3)) {
+                return
+            }
+            
+            let newLunchItem = getMealItemNutritionData(item: allMealItemPermutationsList[0][Int(arc4random_uniform(UInt32(allMealItemPermutationsList[0].count)) + 0)])
+            
+            if (newLunchItem.totalCalories > 0) {
+                currentCarbs += newLunchItem.totalCarbs
+                currentFat += newLunchItem.totalFat
+                currentCalories += newLunchItem.totalCalories
+                currentProtein += newLunchItem.totalProtein
+                currentFiber += newLunchItem.totalFiber
+                lunchCalories += newLunchItem.totalCalories
+                finalLunch.append(newLunchItem)
+            }
+        }
+    }
+    
+    // Generate a meal plan for dinner
+    func generateDinner() {
+        var dinnerCalories = 0
+        for _ in 0...20 {
+            if (dinnerCalories > (Int(calorieGoal)!/3)) {
+                return
+            }
+            
+            let newDinnerItem = getMealItemNutritionData(item: allMealItemPermutationsList[0][Int(arc4random_uniform(UInt32(allMealItemPermutationsList[0].count)) + 0)])
+            
+            if (newDinnerItem.totalCalories > 0) {
+                currentCarbs += newDinnerItem.totalCarbs
+                currentFat += newDinnerItem.totalFat
+                currentCalories += newDinnerItem.totalCalories
+                currentProtein += newDinnerItem.totalProtein
+                currentFiber += newDinnerItem.totalFiber
+                dinnerCalories += newDinnerItem.totalCalories
+                finalLunch.append(newDinnerItem)
+            }
+        }
     }
     
     // Parse the breakfast/lunch/dinner food preferences raw data
@@ -72,30 +146,42 @@ class GeneratedMealPlanViewController: UIViewController {
         
         // Permutating all unique breakfast combos
         for item_one in breakfast {
-            breakfastCombos.append(item_one)
+            var itemOne = item_one
+            if (itemOne.hasPrefix(" ")) { itemOne = String(itemOne.dropFirst()) }
+            breakfastCombos.append(itemOne)
             for item_two in breakfast {
-                if (item_one != item_two) {
-                    breakfastCombos.append(item_one + " " + item_two)
+                var itemTwo = item_two
+                if (itemTwo.hasPrefix(" ")) { itemTwo = String(itemTwo.dropFirst()) }
+                if (itemOne != itemTwo) {
+                    breakfastCombos.append(itemOne + " " + itemTwo)
                 }
             }
         }
         
         // Permutating all unique lunch combos
         for item_one in lunch {
-            lunchCombos.append(item_one)
+            var itemOne = item_one
+            if (itemOne.hasPrefix(" ")) { itemOne = String(itemOne.dropFirst()) }
+            lunchCombos.append(itemOne)
             for item_two in lunch {
-                if (item_one != item_two) {
-                    lunchCombos.append(item_one + " " + item_two)
+                var itemTwo = item_two
+                if (itemTwo.hasPrefix(" ")) { itemTwo = String(itemTwo.dropFirst()) }
+                if (itemOne != itemTwo) {
+                    lunchCombos.append(itemOne + " " + itemTwo)
                 }
             }
         }
         
         // Permutating all unique dinner combos
         for item_one in dinner {
-            dinnerCombos.append(item_one)
+            var itemOne = item_one
+            if (itemOne.hasPrefix(" ")) { itemOne = String(itemOne.dropFirst()) }
+            dinnerCombos.append(itemOne)
             for item_two in dinner {
-                if (item_one != item_two) {
-                    dinnerCombos.append(item_one + " " + item_two)
+                var itemTwo = item_two
+                if (itemTwo.hasPrefix(" ")) { itemTwo = String(itemTwo.dropFirst()) }
+                if (itemOne != itemTwo) {
+                    dinnerCombos.append(itemOne + " " + itemTwo)
                 }
             }
         }
@@ -133,6 +219,7 @@ class GeneratedMealPlanViewController: UIViewController {
             
             let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
             if let responseJSON = responseJSON as? [String: Any] {
+                print("\n\nNEW MEAL ITEM QUERY: " + item)
                 print(responseJSON.description)
                 var capturedCarbs = 0
                 var capturedFat = 0
@@ -172,42 +259,42 @@ class GeneratedMealPlanViewController: UIViewController {
                             let characters = Array(currentSubstring.dropLast())
                             var decodedString = ""
                             for (index, value) in characters.enumerated() { if (index > 1 && index < characters.count - 2) { decodedString += String(value) } }
-                            capturedCarbs = Int(Float(decodedString)!)
+                            capturedCarbs = Int(Float(decodedString) ?? 0)
                         }
                         // Get fat field data
                         if (foundFatData < 3 && foundFatData > 0) { foundFatData += 1 } else if (foundFatData == 3) { foundFatData = 4
                             let characters = Array(currentSubstring.dropLast())
                             var decodedString = ""
                             for (index, value) in characters.enumerated() { if (index > 1 && index < characters.count - 2) { decodedString += String(value) } }
-                            capturedFat = Int(Float(decodedString)!)
+                            capturedFat = Int(Float(decodedString) ?? 0)
                         }
                         // Get calories field data
                         if (foundCaloriesData < 3 && foundCaloriesData > 0) { foundCaloriesData += 1 } else if (foundCaloriesData == 3) { foundCaloriesData = 4
                             let characters = Array(currentSubstring.dropLast())
                             var decodedString = ""
                             for (index, value) in characters.enumerated() { if (index > 1 && index < characters.count - 2) { decodedString += String(value) } }
-                            capturedCalories = Int(Float(decodedString)!)
+                            capturedCalories = Int(Float(decodedString) ?? 0)
                         }
                         // Get protein field data
                         if (foundProteinData < 3 && foundProteinData > 0) { foundProteinData += 1 } else if (foundProteinData == 3) { foundProteinData = 4
                             let characters = Array(currentSubstring.dropLast())
                             var decodedString = ""
                             for (index, value) in characters.enumerated() { if (index > 1 && index < characters.count - 2) { decodedString += String(value) } }
-                            capturedProtein = Int(Float(decodedString)!)
+                            capturedProtein = Int(Float(decodedString) ?? 0)
                         }
                         // Get fiber field data
                         if (foundFiberData < 3 && foundFiberData > 0) { foundFiberData += 1 } else if (foundFiberData == 3) { foundFiberData = 4
                             let characters = Array(currentSubstring.dropLast())
                             var decodedString = ""
                             for (index, value) in characters.enumerated() { if (index > 1 && index < characters.count - 2) { decodedString += String(value) } }
-                            capturedFiber = Int(Float(decodedString)!)
+                            capturedFiber = Int(Float(decodedString) ?? 0)
                         }
                         // Get sugar field data
                         if (foundSugarData < 3 && foundSugarData > 0) { foundSugarData += 1 } else if (foundSugarData == 3) { foundSugarData = 4
                             let characters = Array(currentSubstring.dropLast())
                             var decodedString = ""
                             for (index, value) in characters.enumerated() { if (index > 1 && index < characters.count - 2) { decodedString += String(value) } }
-                            capturedSugar = Int(Float(decodedString)!)
+                            capturedSugar = Int(Float(decodedString) ?? 0)
                         }
                         
                         currentSubstring = ""
